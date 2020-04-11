51a2a05c5d5a1c61851b27beeb77f57945878869
Chelsey Wang <chelseyyywang@gmail.com>
2020-04-11 14:19:42 -0700

    mapbox

--- src/screens/Events.js
+++ src/screens/Events.js
@@ -1,34 +1,41 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import {
-    SafeAreaView,
-    StyleSheet,
-    ScrollView,
-    View,
-    Text,
-    StatusBar,
-    Button, 
-    TouchableOpacity
-  } from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
+import MapboxGL from '@react-native-mapbox-gl/maps';
 
+MapboxGL.setAccessToken(
+  'pk.eyJ1IjoiY2hlbHNleXl3YW5nIiwiYSI6ImNrOHcwNDZvYjA5OHkzbW1jdTVsa3V0NXoifQ.eJ6iKjYTVvEg-6KwOG8hpw',
+);
 
-    class EventsScreen extends React.Component {
-    render() {
-        return (
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  container: {
+    height: 300,
+    width: 300,
+    backgroundColor: 'tomato',
+  },
+  map: {
+    flex: 1,
+  },
+});
+
+export default class EventsScreen extends Component {
+  componentDidMount() {
+    MapboxGL.setTelemetryEnabled(false);
+  }
+
+  render() {
+    return (
+      <View style={styles.page}>
         <View style={styles.container}>
-            <Text style={{color: "white"}}>EVENTS</Text>
+          <MapboxGL.MapView style={styles.map} />
+          <Text>crying</Text>
         </View>
-        )
-    }
-    }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: "#121640", 
-
-    }
-  });
-  export default EventsScreen; 
+      </View>
+    );
+  }
+}
